9b6bd1235c4bd484cd70ee28aa20bdc8aa4c4c7f
Christoph Fabianek <christoph.fabianek@gmail.com>
2026-01-19 23:03:07 +0100

    demo login mit -e SKIP_IDAUSTRA=true

--- script/init.sh
+++ script/init.sh
@@ -11,11 +11,6 @@ then
 	cp config/database_k8s.yml config/database.yml
 	cp db/migrate_pg/* db/migrate/
 fi
-cp config/locales_dao/* config/locales/
-cp public_dao/* public/
-cp app/assets/images_dao/* app/assets/images/
-cp config/initializers_dao/* config/initializers/
-echo "copy complete"
 bundle exec rake db:create
 bundle exec rake active_storage:install
 bundle exec rake active_storage:postgresql:install
